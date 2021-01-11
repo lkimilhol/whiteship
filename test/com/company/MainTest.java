841cc1f139bc0877606181dac32e608888bc30c5
lkimilhol <lkimilhol@gmail.com>
2021-01-11 20:35:18 +0900

    - junit5.4 추가

---
+++ test/com/company/MainTest.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void testExec() {
+        int i = 1;
+        Assertions.assertEquals(1, i);
+    }
+}
