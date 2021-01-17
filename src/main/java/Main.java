acd9217a9aa62c0fd910ff40aaed9e517d3b5607
lkimilhol <lkimilhol@gmail.com>
2021-01-17 15:40:37 +0900

    메이븐 업데이트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
-package com.company;
+import dataStructure.ListNode;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Main");
     }
 }
